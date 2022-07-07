d8c34555300ec5f61346b9904e152d8ec2817673
seungmin Ryu <ojysep9987@naver.com>
2022-07-07 15:51:03 +0900

    AOP BASIC

---
+++ src/main/java/com/example/spring/aop/ParameterAop.java
@@ -0,0 +1,43 @@
+package com.example.spring.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect // AOP로 동작하게함, log를 남기게하는 것
+@Component
+public class ParameterAop {
+
+    // execution안에 들어가는 것은 내가 만든 객체들 경로인듯?
+    @Pointcut("execution(* com.example.spring.aop..*.*(..))")
+    private void pointCut(){
+
+    }
+
+    @Before("pointCut()")
+    public void before(JoinPoint joinPoint){
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        System.out.println(method.getName()); // 메서드 이름 log 찍기
+
+        Object[] args = joinPoint.getArgs();
+
+        for(Object obj : args){
+            System.out.println("type : "+obj.getClass().getSimpleName());
+            System.out.println("value : "+obj);
+        }
+    }
+
+    @AfterReturning(value = "pointCut()", returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj){
+        System.out.println("return obj : "+returnObj);
+    }
+
+
+}
--- src/main/java/com/example/spring/aop/RestApiController.java
+++ src/main/java/com/example/spring/aop/RestApiController.java
@@ -1,9 +1,19 @@
 package com.example.spring.aop;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.spring.UserAop;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/aop")
 public class RestApiController {
+
+    @GetMapping("/get/{id}")
+    public String get(@PathVariable Long id, @RequestParam String name){
+        return id+" "+name;
+    }
+
+    @PostMapping("/post")
+    public UserAop post(@RequestBody UserAop userAop){
+        return userAop;
+    }
 }
