61d0cd3ab01dd662d21691aeb3b47dfa568ee169
seungmin Ryu <ojysep9987@naver.com>
2022-07-11 14:44:55 +0900

    Object Mapper

---
+++ src/main/java/objectMapperDto/ObjectMapperTest.java
@@ -0,0 +1,65 @@
+package objectMapperDto;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ObjectMapperTest {
+    public static void main(String[] args) throws JsonProcessingException {
+        System.out.println("om");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+
+        User user = new User();
+        user.setName("유승민");
+        user.setAge(24);
+
+        Car car1 = new Car();
+        car1.setName("audi");
+        car1.setCarNumber("11가 1234");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("Q5");
+        car2.setCarNumber("22가 4321");
+        car2.setType("SUV");
+
+        List<Car> carList = Arrays.asList(car1, car2);
+        user.setCars(carList);
+
+//        System.out.println(user);
+
+        String json = objectMapper.writeValueAsString(user);
+        System.out.println(json);
+
+        // json 읽어오기
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String _name = jsonNode.get("name").asText();
+        int _age = jsonNode.get("age").asInt();
+        System.out.println("name : "+_name);
+        System.out.println("age : "+_age);
+
+        JsonNode cars = jsonNode.get("cars");
+        ArrayNode arrayNode = (ArrayNode)cars;
+
+        // object를 받아서 원하는 타입으로 변환시켜줌
+        List<Car> _cars = objectMapper.convertValue(arrayNode, new TypeReference<List<Car>>() {});
+        System.out.println(_cars);
+
+        // json value값 바꾸기
+        ObjectNode objectNode = (ObjectNode) jsonNode;
+        objectNode.put("name","steve");
+        objectNode.put("age",25);
+
+        // json 예쁘기 출력하기기
+        System.out.println(objectNode.toPrettyString());
+    }
+}
