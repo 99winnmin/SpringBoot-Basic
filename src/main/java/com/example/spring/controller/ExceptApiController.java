c7ae5dd696ff5676e464a63f0efacb060cf21c16
seungmin Ryu <ojysep9987@naver.com>
2022-07-11 21:06:52 +0900

    ExceptionHandling 모범사례

--- src/main/java/com/example/spring/controller/ExceptApiController.java
+++ src/main/java/com/example/spring/controller/ExceptApiController.java
@@ -3,18 +3,27 @@ package com.example.spring.controller;
 import com.example.spring.dto.ExceptionUser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.*;
 
 @RestController
 @RequestMapping("/api")
+@Validated // @Validated 로 required = false가 없어도 유효성 검사를 하게함
 public class ExceptApiController {
 
-    // required = false 는 requestParam이 없어도 되지만 null이 들어가게됨됨
+    // required = false 는 requestParam이 없어도 되지만 null이 들어가게됨
    @GetMapping("/except/get")
-    public ExceptionUser get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public ExceptionUser get(
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age){
         ExceptionUser exceptionUser = new ExceptionUser();
         exceptionUser.setName(name);
         exceptionUser.setAge(age);
@@ -32,9 +41,9 @@ public class ExceptApiController {
 
     // Controller 안에 ExceptionHandler를 코딩하게 되면 이 class 안에서 발생하는 예외에만 관여하게됨
     // GlobalHandler 보다 우선순위가 높아서 여기서 예외처리하고 끝
-   @ExceptionHandler(value = MethodArgumentNotValidException.class)
+   /*@ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
        System.out.println("api controller");
        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+    }*/
 }
