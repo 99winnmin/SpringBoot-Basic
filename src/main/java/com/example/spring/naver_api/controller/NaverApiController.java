815ce6b20dcdb687a70a81eeb13e078abc3e562e
seungmin Ryu <ojysep9987@naver.com>
2022-07-13 23:43:49 +0900

    NAVER API 사용해보기

---
+++ src/main/java/com/example/spring/naver_api/controller/NaverApiController.java
@@ -0,0 +1,52 @@
+package com.example.spring.naver_api.controller;
+
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+
+@RestController
+@RequestMapping("/naver-api")
+public class NaverApiController {
+
+    // https://openapi.naver.com/v1/search/local.json
+    // ?query=%EC%A3%BC%EC%8B%9D
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/search")
+    public String naver(){
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query","백엔드")
+                .queryParam("display",10)
+                .queryParam("start",1)
+                .queryParam("sort","random")
+                .encode(Charset.forName("UTF-8")) // 여기서 query 내용 encoding 해줌
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","ujIgIQ6vXYCYragbO5wS")
+                .header("X-Naver-Client-Secret","s1qobZc4uV")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
+}
