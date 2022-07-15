0d9f71529f2986042662bdecd93139d519000b37
seungmin Ryu <ojysep9987@naver.com>
2022-07-15 21:47:12 +0900

    code 정리

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -3,18 +3,14 @@ package com.example.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.Base64;
 
 // @ServletComponentScan // 특정 class에만 filter를 적용시키고 싶을 때
 @SpringBootApplication
-@EnableAsync
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        //System.out.println(Base64.getEncoder().encodeToString("ojysep9987@gmail.com".getBytes()));
     }
 
 }
